6f735a153e345729f617c5205be453c8dec19018
khseob0715 <khseob0715@naver.com>
2017-10-19 22:32:08 +0900

    17.10.19 Project Start
    * Youtube 360도 영상을 가져와서 Vive에서 볼수 있도록 할 것이다.
    
    * Quicktime tool을 이용하여 movietexture로 import하는 것은 가능하다
    
    * 다만 너무 느리고 화질이 안 좋아 진다. 이 부분을 수정해야한다.

---
+++ 360YoutubeVR/Assets/Script/VideoController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class VideoController : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        MovieTexture MOVIE_TEXTURE = ((MovieTexture)GetComponent<Renderer>().material.mainTexture);
+               
+        AudioSource movieAudio = GetComponent<AudioSource>();
+        movieAudio.clip = MOVIE_TEXTURE.audioClip;
+        
+        MOVIE_TEXTURE.Play();
+        movieAudio.Play();
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
